be2aa93df7ce385af4a0ed5d82497bd6f83ea493
jtrou1 <33576771+jtrou1@users.noreply.github.com>
2020-06-19 14:38:10 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function sendme_blog1() {
+    window.location.replace("/html/Blog_Post1.html");
+}
+function sendme_blog2() {
+    window.location.replace("/html/Blog_Post2.html");
+}
+function sendme_blog3() {
+    window.location.replace("/html/Blog_Post3.html");
+}
